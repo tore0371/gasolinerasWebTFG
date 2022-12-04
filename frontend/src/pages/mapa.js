cd3fd7e05c3137fc2c2840356a668378a8493cc6
srodriguez <srodriguez@vexiza.net>
2022-12-04 16:27:10 +0000

    marker in map

--- frontend/src/pages/mapa.js
+++ frontend/src/pages/mapa.js
@@ -3,35 +3,28 @@ import { Map, TileLayer, Marker, Popup } from "react-leaflet-universal";
 import "leaflet/dist/leaflet.css";
 import Header from "../layouts/Header";
 import Footer from "../layouts/Footer";
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import { Icon } from 'leaflet'
+
 
 export default () => {
-  React.useEffect(() => {
-    const L = require("leaflet");
 
-    delete L.Icon.Default.prototype._getIconUrl;
 
-    L.Icon.Default.mergeOptions({
-      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-      iconUrl: require("leaflet/dist/images/marker-icon.png"),
-      shadowUrl: require("leaflet/dist/images/marker-shadow.png")
-    });
-  }, []);
-
-  return (
-    <>
-    <Header />
-    <Map center={[40.4165, -3.70256]} zoom={6} style={{ height: "85vh" }}>
-      <TileLayer
-        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </Map>
-    <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Map center={[40.4165, -3.70256]} zoom={6} style={{ height: "85vh" }}>
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[40.4165, -3.70256]} icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41] })} >
+                    <Popup>
+                        Madrid
+                    </Popup>
+                </Marker>
+            </Map>
+            <Footer />
+        </>
+    );
 };
